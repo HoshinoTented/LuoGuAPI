c400bcba033c4e5b63c40a26540ea562111316fe
HoshinoTented <limbolrain@gmail.com>
2018-10-20 09:19:59 +0800

    [ CI ] QAQ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.internal.HasConvention
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -6,6 +8,8 @@ plugins {
 
 group = "org.hoshino9"
 version = "0.0.1"
+
+val SourceSet.kotlin get() = (this as HasConvention).convention.getPlugin(KotlinSourceSet::class).kotlin
 
 repositories {
 	jcenter()
@@ -21,4 +25,14 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "1.8"
+}
+
+val sourcesJar = task<Jar>("sourcesJar") {
+	from(sourceSets.getByName("main").kotlin)
+	classifier = "sources"
+}
+
+artifacts {
+	operator fun String.invoke(obj : Any) = add(this, obj)
+	"archives"(sourcesJar)
 }
