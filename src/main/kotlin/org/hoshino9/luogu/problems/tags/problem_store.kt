a65128df8d037ec97a90fa4476756cf28ff9252a
HoshinoTented <limbolrain@gmail.com>
2018-11-04 00:48:37 +0800

    fixed bug

--- src/main/kotlin/org/hoshino9/luogu/problems/tags/problem_store.kt
+++ src/main/kotlin/org/hoshino9/luogu/problems/tags/problem_store.kt
@@ -4,10 +4,10 @@ package org.hoshino9.luogu.problems.tags
 
 import org.hoshino9.luogu.LuoGuTag
 
-abstract class ProblemStore(text : String, data : Int) : LuoGuTag(text, data)
+sealed class ProblemStore(text : String, data : Int) : LuoGuTag(text, data)
 
-sealed class LuoGu : ProblemStore("洛谷题库", 1)
-sealed class CodeForces : ProblemStore("CodeForces", 13)
-sealed class SPOJ : ProblemStore("SPOJ", 14)
-sealed class AtCoder : ProblemStore("AtCoder", 15)
-sealed class UVa : ProblemStore("UVa", 16)
+object LuoGu : ProblemStore("洛谷题库", 1)
+object CodeForces : ProblemStore("CodeForces", 13)
+object SPOJ : ProblemStore("SPOJ", 14)
+object AtCoder : ProblemStore("AtCoder", 15)
+object UVa : ProblemStore("UVa", 16)
