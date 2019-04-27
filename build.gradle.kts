e69e89b347495ff6dab1a760348f9d89631a11d5
HoshinoTented <limbolrain@gmail.com>
2019-04-27 17:50:11 +0800

    qwq?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-
 plugins {
 	kotlin("jvm") version "1.3.30"
 }
@@ -13,30 +11,10 @@ allprojects {
 		plugin("kotlin")
 	}
 
-	group = parent?.group?.let { prefix -> "$prefix.$name" } ?: "org.hoshino9"
-	version = parent?.version ?: "0.0.1"
+	group = "org.hoshino9"
+	version = "0.0.2"
 
 	repositories {
 		jcenter()
-	}
-
-	if (this.path != ":luogu") {
-		sourceSets {
-			val names = listOf("main", "test")
-
-			names.forEach { name ->
-				getByName(name) {
-					withConvention(KotlinSourceSet::class) {
-						kotlin.srcDir("$name/kotlin")
-
-						resources.srcDir("$name/resources")
-					}
-				}
-			}
-		}
-
-		dependencies {
-			compile(project(":luogu"))
-		}
 	}
 }
