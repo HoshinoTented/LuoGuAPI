87844cf7bdaff569ab2fb8d131802d2692b05aac
HoshinoTented <limbolrain@gmail.com>
2019-07-01 16:50:45 +0800

    fix ci

--- extensions/problem/main/kotlin/org/hoshino9/luogu/problem/experimental/LoggedProblem.kt
+++ extensions/problem/main/kotlin/org/hoshino9/luogu/problem/experimental/LoggedProblem.kt
@@ -6,13 +6,13 @@ import org.hoshino9.luogu.utils.HttpClient
 interface LoggedProblem : Problem {
 	open class Factory(pid: String, client: HttpClient) : Problem.Factory(pid, client), LoggedProblem {
 		override val isAccepted: Boolean
-			get() = data["isAccepted"].asBoolean
+			get() = data !!["isAccepted"].asBoolean
 
 		override val score: Int
-			get() = data["score"].asInt
+			get() = data !!["score"].asInt
 
 		override val showScore: Boolean
-			get() = data["showScore"].asBoolean
+			get() = data !!["showScore"].asBoolean
 	}
 
 	val isAccepted: Boolean
