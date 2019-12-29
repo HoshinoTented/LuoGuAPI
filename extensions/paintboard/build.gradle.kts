a57645e4f3f788d3a0acb91e401ec8691d517725
HoshinoTented <limbolrain@gmail.com>
2019-12-29 19:59:30 +0800

    删除冬日绘板代码

--- extensions/paintboard/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-dependencies {
-	compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.0")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions.jvmTarget = "1.8"
-}
