ac85702ab0d2d8cb975e51e769a228818619175e
HoshinoTented <limbolrain@gmail.com>
2020-03-08 19:32:11 +0800

    add some extra functions

--- extensions/training/main/kotlin/org/hoshino9/luogu/training/ext.kt
+++ extensions/training/main/kotlin/org/hoshino9/luogu/training/ext.kt
@@ -2,9 +2,34 @@
 
 package org.hoshino9.luogu.training
 
+import io.ktor.client.call.receive
+import org.hoshino9.luogu.LuoGu
 import org.hoshino9.luogu.utils.HttpClient
+import org.hoshino9.luogu.utils.apiPost
 import org.hoshino9.luogu.utils.emptyClient
 
 fun BaseTraining.lift(client: HttpClient = emptyClient): TrainingInfo {
 	return TrainingInfoPage(id).info
+}
+
+fun LuoGu.officialTraining(page: Int = 1): TrainingListPage {
+	return TrainingListPage(page, TrainingListPage.Type.Official, client)
+}
+
+fun LuoGu.publicTraining(page: Int = 1): TrainingListPage {
+	return TrainingListPage(page, TrainingListPage.Type.Public, client)
+}
+
+fun LuoGu.training(id: Int): TrainingInfoPage {
+	return TrainingInfoPage(id, client)
+}
+
+internal fun url(id: Int, mark: Boolean) = "api/training/${if (mark) "mark" else "unmark"}/$id"
+
+suspend fun LuoGu.markTraining(id: Int) {
+	apiPost(url(id, true)).receive<String>()
+}
+
+suspend fun LuoGu.unmarkTraining(id: Int) {
+	apiPost(url(id, false)).receive<String>()
 }
